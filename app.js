b1daf5121f04ed112a6112f0c806350543e038e5
MAESTROOOOO <maestrovirtuoso45@gmail.com>
2020-03-12 15:57:50 +0000

    xd

---
+++ app.js
@@ -0,0 +1,30 @@
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+const name = function(par){
+    return par.length;
+}
+
+console.log(name(myAlphabet));
+
+var Planets = ['Earth', 'Jupiter', 'Saturn', 'Mars', 'Uranus'];
+
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+var wowDatatypes = [3, 'int', true];
+
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+var hello = "Hello";
+
+function helloworld()
+{
+    return hello + " World!";
+}
+
+console.log(helloworld());
